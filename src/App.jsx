b6743c001ff67e779054303a3387b482d6f723ef
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-05-09 18:47:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import About from "./components/About";
+import Workouts from "./components/Workouts";
+import Pricing from "./components/Pricing";
+import Community from "./components/Community";
+import Faq from "./components/Faq";
+import Join from "./components/Join";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
+      <Header />
+      <Banner />
+      <About />
+      <Workouts />
+      <Pricing />
+      <Community />
+      <Faq />
+      <Join />
+      <Footer />
+      <div className="h-[4000px]"></div>
+    </div>
+  );
+}
+
+export default App;
